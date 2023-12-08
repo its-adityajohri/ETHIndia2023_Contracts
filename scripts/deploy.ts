a135df5b273371f9a514644ca02c5dc45c433878
Akshay Meher <akshay_111meher@hotmail.com>
2023-12-08 21:53:33 +0400

    Token Bridge Deployment Script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,10 +2,17 @@ import { ethers, run } from "hardhat";
 import {
   Inbox,
   Inbox__factory,
+  MyToken,
+  MyToken__factory,
   Outbox,
   Outbox__factory,
+  TokenBridge,
+  TokenBridge__factory,
 } from "../typechain-types";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import BigNumber from "bignumber.js";
+
+const tokenSupply = new BigNumber(10).pow(18).multipliedBy(1000000000000);
 
 async function main() {
   const signers = await ethers.getSigners();
@@ -19,6 +26,13 @@ async function main() {
   const inbox = await deployInbox(admin);
   const outbox = await deployOutbox(admin);
 
+  const name = "Decimal Demo Bridge token";
+  const symbol = "DDBT";
+
+  const tokenInstance = await deployToken(name, symbol, admin);
+
+  const bridgeInstance = await deployTokenBridge(inbox, outbox, admin);
+
   verificationResult = await run("verify:verify", {
     address: await inbox.getAddress(),
     constructorArguments: [await admin.getAddress()],
@@ -27,6 +41,25 @@ async function main() {
   verificationResult = await run("verify:verify", {
     address: await outbox.getAddress(),
     constructorArguments: [],
+  });
+
+  verificationResult = await run("verify:verify", {
+    address: await tokenInstance.getAddress(),
+    constructorArguments: [
+      name,
+      symbol,
+      await admin.getAddress(),
+      tokenSupply.toFixed(0),
+    ],
+  });
+
+  verificationResult = await run("verify:verify", {
+    address: await tokenInstance.getAddress(),
+    constructorArguments: [
+      await inbox.getAddress(),
+      await outbox.getAddress(),
+      await admin.getAddress(),
+    ],
   });
 
   return "Done";
@@ -42,6 +75,29 @@ async function deployInbox(admin: HardhatEthersSigner): Promise<Inbox> {
 async function deployOutbox(admin: HardhatEthersSigner): Promise<Outbox> {
   const outbox = await new Outbox__factory(admin).deploy();
   return outbox;
+}
+
+async function deployToken(
+  name: string,
+  symbol: string,
+  admin: HardhatEthersSigner,
+): Promise<MyToken> {
+  const tokenInstance = await new MyToken__factory(admin).deploy(
+    name,
+    symbol,
+    await admin.getAddress(),
+    tokenSupply.toFixed(0),
+  );
+  return tokenInstance;
+}
+
+async function deployTokenBridge(
+  inbox: Inbox,
+  outbox: Outbox,
+  admin: HardhatEthersSigner,
+): Promise<TokenBridge> {
+  const instance = await new TokenBridge__factory(admin).deploy(inbox, outbox);
+  return instance;
 }
 
 main().then(console.log).catch(console.log);
